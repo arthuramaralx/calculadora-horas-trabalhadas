0fd99f148287ef729a93afcf9a9dc579be11aadc
Arthur Amaral <arthuramaralx@outlook.com>
2023-06-26 12:15:11 -0300

    create class department and hourcontract

---
+++ app/src/entities/HourContract.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+    
+    private Date date;
+    private Double valuesPerHour;
+    private Integer hours;
+
+  
+    public HourContract(Date date, Double valuesPerHour, Integer hours) {
+        this.date = date;
+        this.valuesPerHour = valuesPerHour;
+        this.hours = hours;
+    }
+
+
+    public Date getDate() {
+        return date;
+    }
+
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+    public Double getValuesPerHour() {
+        return valuesPerHour;
+    }
+
+
+    public void setValuesPerHour(Double valuesPerHour) {
+        this.valuesPerHour = valuesPerHour;
+    }
+
+
+    public Integer getHours() {
+        return hours;
+    }
+
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    };
+
+    
+    
+}
